9209efcb67a38377b7e2fa46067e6ab3d507e8d9
hridoyhalder1 <hridoyhalder91@gmail.com>
2022-11-16 01:06:19 +0600

    appointmentOptions data loded

--- index.js
+++ index.js
@@ -10,14 +10,28 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-user:doctorsPortal
-password:LtyQGLzKpuSJ2NAj
-
-const uri = "mongodb+srv://<username>:<password>@cluster0.zkcjl29.mongodb.net/?retryWrites=true&w=majority";
+// connect with mongodb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zkcjl29.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+async function run(){
+    try{
+        const appointmentOptionsCollection = client.db('doctorsPortal').collection('appointmentOptions');
+
+        app.get('/appointmentOptions', async(req, res) => {
+            const query = {};
+            const options = await appointmentOptionsCollection.find(query).toArray();
+            res.send(options);
+
+        })
+
+    }
+    finally{
+
+    }
+}
+run().catch(console.log)
 
 
 app.get('/', async(req, res) => {
